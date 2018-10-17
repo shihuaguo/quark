64346c73697763402e3abe15cc6963036cc14d32
shihuaguo <shihuaguo@mindai.com>
2018-10-17 16:51:10 +0800

    add openstore-config project

---
+++ openstore-config/src/main/java/com/github/openstore/config/OpenstoreConfigApplication.java
@@ -0,0 +1,14 @@
+package com.github.openstore.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class OpenstoreConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenstoreConfigApplication.class, args);
+    }
+}
