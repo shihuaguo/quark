53a51e0cd0f7c243a08bf4034560db0cda54c91f
shihuaguo <shihuaguo@mindai.com>
2018-10-24 18:34:20 +0800

    add openstore-common project

---
+++ openstore-gateway/src/main/java/com/github/openstore/gateway/filter/AccessFilter.java
@@ -0,0 +1,47 @@
+package com.github.openstore.gateway.filter;
+
+import com.github.openstore.common.constant.SecurityConstants;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.FORM_BODY_WRAPPER_FILTER_ORDER;
+
+/**
+ * @author shihuaguo
+ * @email huaguoshi@gmail.com
+ * @date 2018-10-24
+ **/
+public class AccessFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return FilterConstants.PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return FORM_BODY_WRAPPER_FILTER_ORDER - 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.set("startTime", System.currentTimeMillis());
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            RequestContext requestContext = RequestContext.getCurrentContext();
+            requestContext.addZuulRequestHeader(SecurityConstants.USER_HEADER, authentication.getName());
+            requestContext.addZuulRequestHeader(SecurityConstants.ROLE_HEADER,  StringUtils.join(authentication.getAuthorities().iterator(), ","));
+        }
+        return null;
+    }
+}
--- service-feign/src/main/java/com/openstore/service/feign/SchedualServiceHiHystric.java
+++ service-feign/src/main/java/com/openstore/service/feign/SchedualServiceHiHystric.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SchedualServiceHiHystric implements SchedualServiceHi {
-    @Override
     public String sayHiFromClientOne(String name) {
         return "sorry "+name;
     }
