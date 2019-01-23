b2e81a420f3af146b9fa1af0590a607f801371be
shihuaguo <shihuaguo@mindai.com>
2019-01-23 17:35:26 +0800

    use spring cloud gateway to replace zuul

--- quark-gateway/src/main/java/com/github/quark/gateway/QuarkGatewayApplication.java
+++ quark-gateway/src/main/java/com/github/quark/gateway/QuarkGatewayApplication.java
@@ -1,35 +1,32 @@
 package com.github.quark.gateway;
 
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.client.RestTemplate;
 
-@EnableZuulProxy
-@EnableFeignClients
+//@EnableZuulProxy
+//@EnableFeignClients
 //@EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringCloudApplication
-@ComponentScan(basePackages = {"com.github.quark.gateway", "com.github.quark.common"})
+@SpringBootApplication
+//@ComponentScan(basePackages = {"com.github.quark.gateway"})
+@EnableApolloConfig
 public class QuarkGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuarkGatewayApplication.class, args);
     }
-    @Bean
-    LoadBalancerInterceptor loadBalancerInterceptor(LoadBalancerClient loadBalance){
-        return new LoadBalancerInterceptor(loadBalance);
-    }
+//    @Bean
+//    LoadBalancerInterceptor loadBalancerInterceptor(LoadBalancerClient loadBalance){
+//        return new LoadBalancerInterceptor(loadBalance);
+//    }
+//
+//    @Bean
+//    public RestTemplate restTemplate() {
+//        return new RestTemplate();
+//    }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+//    @Bean
+//    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient) {
+//        return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
+//    }
 }
