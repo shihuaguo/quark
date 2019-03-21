7bd3a80b8afbf2819c59e7fab496f451c78dd5c6
shihuaguo <shihuaguo@mindai.com>
2019-03-21 13:02:33 +0800

    优化quark-eureke

--- quark-eureka/src/main/java/com/github/quark/eureka/QuarkEurekeServer.java
+++ quark-eureka/src/main/java/com/github/quark/eureka/QuarkEurekeServer.java
@@ -1,16 +1,15 @@
 package com.github.quark.eureka;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-@EnableApolloConfig
-public class EurekaServerApplication {
+//@EnableApolloConfig
+public class QuarkEurekeServer {
 
     public static void main(String[] args) {
-        SpringApplication.run(EurekaServerApplication.class, args);
+        SpringApplication.run(QuarkEurekeServer.class, args);
     }
 }
