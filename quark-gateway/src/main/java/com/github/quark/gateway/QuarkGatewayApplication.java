a8d96d7b66191e0d0810ce64f5c685e6f3c49f45
shihuaguo <shihuaguo@mindai.com>
2019-03-27 16:43:00 +0800

    gateway项目初始化

--- quark-gateway/src/main/java/com/github/quark/gateway/QuarkGatewayApplication.java
+++ quark-gateway/src/main/java/com/github/quark/gateway/QuarkGatewayApplication.java
@@ -1,32 +1,12 @@
 package com.github.quark.gateway;
 
-import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableZuulProxy
-//@EnableFeignClients
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
-//@ComponentScan(basePackages = {"com.github.quark.gateway"})
-@EnableApolloConfig
 public class QuarkGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuarkGatewayApplication.class, args);
     }
-//    @Bean
-//    LoadBalancerInterceptor loadBalancerInterceptor(LoadBalancerClient loadBalance){
-//        return new LoadBalancerInterceptor(loadBalance);
-//    }
-//
-//    @Bean
-//    public RestTemplate restTemplate() {
-//        return new RestTemplate();
-//    }
-
-//    @Bean
-//    public RouteDefinitionLocator discoveryClientRouteDefinitionLocator(DiscoveryClient discoveryClient) {
-//        return new DiscoveryClientRouteDefinitionLocator(discoveryClient);
-//    }
 }
