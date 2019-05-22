7b99646df7550f7f1aaf162251e3b9e74291bc3b
shihuaguo <shihuaguo@mindai.com>
2019-05-22 16:14:18 +0800

    service-admin add main class

---
+++ quark-service/quark-service-admin/src/main/java/com/github/quark/service/AdminApplication.java
@@ -0,0 +1,16 @@
+package com.github.quark.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shihuaguo
+ * @email huaguoshi@gmail.com
+ * @date 2019-05-22
+ **/
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args){
+        SpringApplication.run(AdminApplication.class, args);
+    }
+}
